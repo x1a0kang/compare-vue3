5a618701386ce411786c7e150f7b1800380da4a7
x1a0kang <18810298505@163.com>
2025-01-16 21:51:44 +0800

    对比列表按钮外观优化

--- pages/compare/compareList.vue
+++ pages/compare/compareList.vue
@@ -3,11 +3,16 @@
 		<custom-nav-bar title="对比"></custom-nav-bar>
 		<view class="line" v-for="item in compareList" :key="item.productId">
 			<productPreview :item="item"></productPreview>
-			<button class="delete" @click="deleteOne(item)">删除</button>
+			<view class="delete" @click="deleteOne(item)">
+				<uni-icons type="trash"></uni-icons>
+			</view>
 		</view>
 
 		<view class="bottom">
-			<button class="deleteAll" @click="deleteAll()">清空</button>
+			<view class="deleteAll" @click="deleteAll()">
+				<uni-icons type="trash-filled"></uni-icons>
+				清空
+			</view>
 			<button class="compareAll" @click="compareAll()">对比</button>
 		</view>
 	</view>
@@ -52,22 +57,46 @@
 	.line {
 		display: flex;
 		align-items: center;
-		justify-content: center;
+		position: relative;
 
 		.delete {
+			position: absolute;
 			display: flex;
+			flex-direction: column;
 			align-items: center;
-			height: 80rpx;
+			height: 50rpx;
+			width: 50rpx;
+			right: 20rpx;
+			bottom: 20rpx;
+			justify-content: center;
 		}
 	}
 
 	.bottom {
 		display: flex;
 		position: fixed;
+		left: 0;
+		right: 0;
 		bottom: var(--window-bottom);
+		align-items: center;
+		justify-content: center;
 
-		.deleteAll {}
+		.deleteAll {
+			display: flex;
+			flex-direction: column;
+			width: 80rpx;
+			height: 80rpx;
+			align-items: center;
+			justify-content: center;
+			margin: 0 10rpx;
+			// background: red;
+		}
 
-		.compareAll {}
+		.compareAll {
+			width: 150rpx;
+			margin-right: 0;
+			background: linear-gradient(90deg, #ebb8e9, #bbebea, #d1ebaa);
+			border-radius: 10rpx;
+		}
 	}
 </style>
