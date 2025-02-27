4973d8b36dedc7b3be405943977ff4d42f98fa14
x1a0kang <18810298505@163.com>
2025-02-27 18:26:55 +0800

    网格预感加入定位和使用场景tag

--- components/gridContent/gridContent.vue
+++ components/gridContent/gridContent.vue
@@ -4,9 +4,15 @@
 		<view class="box shadow" v-for="item in arrs" :key="item.productId">
 			<view class="jump-button" @click="jump(item)">
 				<image class="image" :src="item.imageList[0]" mode="aspectFill"></image>
-				<view class="mask">{{item.brand}} {{item.name}}</view>
-				<!-- <view class="mask">{{item.name}}</view> -->
-				<!-- <view class="mask">{{item.price}}元</view> -->
+
+				<view class="text">{{item.brand}} {{item.name}}</view>
+				<view class="position">{{item.position}}</view>
+
+				<view class="scenarioList">
+					<view class="scenario" v-for="scenario in item.scenarioList" :key="scenario">
+						{{scenario}}
+					</view>
+				</view>
 			</view>
 
 			<view class="add-button">
@@ -44,35 +50,65 @@
 
 <style lang="scss" scoped>
 	.content {
-		// margin-top: 5rpx;
-		// padding: 0 10rpx;
 		display: grid;
 		gap: 10rpx;
 		grid-template-columns: repeat(2, 1fr);
 		// background-color: red;
 
 		.box {
-			border-radius: 10rpx;
+			border-radius: 20rpx;
 			overflow: hidden;
 			position: relative;
-			padding-bottom: 10rpx;
+			padding-bottom: 5rpx;
 			background-color: white;
-
-			.jump-button {
-				height: 100%;
-			}
-
-			.image {
-				width: 100%;
-				height: 350rpx;
-			}
-
-			.mask {
-				padding: 5rpx 5rpx 5rpx 20rpx;
-				width: 80%;
-			}
 		}
-		
+
+		.jump-button {
+			height: 100%;
+		}
+
+		.image {
+			width: 100%;
+			height: 350rpx;
+		}
+
+		.text {
+			padding: 5rpx 10rpx 5rpx 20rpx;
+			width: 100%;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			text-wrap: nowrap;
+		}
+
+		.scenarioList {
+			padding: 5rpx 10rpx 5rpx 10rpx;
+			width: 90%;
+			display: flex;
+			gap: 5rpx;
+			flex-wrap: wrap;
+			// background: red;
+		}
+
+		.scenario {
+			width: fit-content;
+			background: $theme-color;
+			color: white;
+			border-radius: 50rpx;
+			font-size: 14px;
+			padding: 5rpx 15rpx;
+		}
+
+		.position {
+			position: absolute;
+			top: 8rpx;
+			right: 10rpx;
+			background: $theme-color;
+			color: white;
+			border-radius: 50rpx;
+			padding: 5rpx 20rpx;
+			text-align: center;
+		}
+
 		.add-button {
 			position: absolute;
 			bottom: 5rpx;
